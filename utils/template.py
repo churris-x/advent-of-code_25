2e34f37e3aeddefd583a07d326abd9c9e3c3a471
Francisco Altimari <francisco@altimari.nl>
2025-11-30 22:27:43 +0100

    Add logic to fetch from aoc and create templates

---
+++ utils/template.py
@@ -0,0 +1,27 @@
+import os
+eg = open(f'{os.path.dirname(__file__)}/eg.txt').read().strip()
+input = open(f'{os.path.dirname(__file__)}/input.txt').read().strip()
+
+# Part 1 ----------------------------------------------------------------------
+
+def placeholder(input):
+    return input
+
+print('1) eg: ',    placeholder(eg))
+# print('1) input: ', placeholder(input))
+
+# Part 2 ----------------------------------------------------------------------
+
+# def placeholder(input):
+#     return input
+
+# print('2) eg:    ',    placeholder(eg))
+# print('2) input: ', placeholder(input))
+
+'''
+Wrong guesses:
+
+Correct:
+    1)
+    2)
+'''
