148e8a8c0b3d4a6924f4d21d0448df619313084e
Francisco Altimari <francisco@altimari.nl>
2025-12-02 17:51:16 +0100

    Solve D1P1

--- 1/solution.js
+++ 1/solution.js
@@ -4,10 +4,28 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
 // Part 1 ---------------------------------------------------------------------
 
-const placeholder = input => input;
+/*
+          v       x
+-----------------------
 
-console.log('1) eg:    ', placeholder(eg));
-// console.log('1) input: ', placeholder(input));
+Dial starts at 50
+
+0 - 99
+
+How many times dial points exaclty at 0
+
+*/
+
+const moveDial = input => input
+    .split('\n')
+    .map(i => [i[0] == 'R' ? 1 : -1 , Number(i.substring(1))])
+    .reduce(([position, total], [heading, ticks]) => {
+        const newPosition = (((position + (heading * ticks)) % 100) + 100) % 100;
+        return [newPosition, newPosition === 0 ? total + 1 : total];
+    }, [50, 0]);
+
+// console.log('1) eg:    ', moveDial(eg));
+// console.log('1) input: ', moveDial(input));
 
 // Part 2 ---------------------------------------------------------------------
 
@@ -20,6 +38,6 @@ console.log('1) eg:    ', placeholder(eg));
 Wrong guesses:
 
 Correct:
-    1) 
+    1) 1055
     2) 
 */
